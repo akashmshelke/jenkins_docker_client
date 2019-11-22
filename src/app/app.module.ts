dca24752dfb5cb307e6598613ce19308c917f453
Akash Shelke <akash.shelke@sunbeaminfo.com>
2019-11-22 06:37:14 +0530

    added more products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GetproductComponent } from './getproduct/getproduct.component';
 import { HttpModule } from '@angular/http';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GetproductComponent,
-    TestComponent
+    
   ],
   imports: [
     BrowserModule,
